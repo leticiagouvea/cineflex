0a7a0ea5ad48847c4e6267ec84f1d17f9d6f2ce7
leticiagouvea <letfgouvea@gmail.com>
2022-10-05 03:06:10 -0300

    primeiro commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import GlobalStyle from "../assets/style/GlobalStyle";
+
+export default function App() {
+    return (
+        <>
+            <GlobalStyle />
+            <h1>Teste</h1>
+        </>
+    )
+}
